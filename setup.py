66f40ace138460cf09a76da2d0d39b25ae67adcd
vP3nguin <leon@vp3ngu.in>
2022-10-19 09:24:11 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+import setuptools
+
+with open("require.cfg") as req:
+    install_requires = [line.strip() for line in req if line.strip()]
+
+setuptools.setup(
+    name="pynetcm",
+    version="0.1",
+    author="vP3nguin",
+    url="https://github.com/vP3nguin/pynetcm",
+    keywords=["traffic control", "bandwidth", "latency", "packet loss"],
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "pynet-set=pynetcm.set:main",
+            "pynet-del=pynetcm.del:main",
+            "pynet-show=pynetcm.show:main",
+        ],
+    }
+)
